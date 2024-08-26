5ced78738f0ca0f5a5de2eed2c8ed82ac94d9f98
breezlia <109561887+breezlia@users.noreply.github.com>
2024-08-25 20:10:39 -0700

    first commit v5 template python

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       jroy                                                         #
+# 	Created:      8/25/2024, 7:43:49 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
