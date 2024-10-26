28bf7a7bb8e3f60eee59bfec068213c7c555a1c2
Razz1027 <boyillasaanvi8@gmail.com>
2024-10-26 14:23:05 -0700

    We fixed the user control

--- src/main.py
+++ src/main.py
@@ -15,87 +15,53 @@ brain=Brain()
 
 brain.screen.print("Hello V5")
 
-motor_r1 = Motor(Ports.PORT1, GearSetting.RATIO_6_1, False)
-motor_r2 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
-motor_r3 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, False)
+motor_r1 = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)
+motor_r2 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
+motor_r3 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
 right_drive = MotorGroup(motor_r1, motor_r2, motor_r3)
+
+motor_intake = Motor(Ports. PORT7, GearSetting. Ratio_6_1, False))
 
 motor_l1 = Motor(Ports.PORT4, GearSetting.RATIO_6_1, False)
 motor_l2 = Motor(Ports.PORT5, GearSetting.RATIO_6_1, False)
 motor_l3 = Motor(Ports.PORT6, GearSetting.RATIO_6_1, False)
 left_drive = MotorGroup(motor_l1, motor_l2, motor_l3)
 
-right_drive.spin(FORWARD, 50, RPM)
-
-right_drive.stop
-
 drivetrain = (left_drive, right_drive)
 
-myVariable = 0
 
-def when_started1():
-    global myVariable
-    drivetrain.drive_for(FORWARD, 200, MM)
-    wait(7, SECONDS)
-    drivetrain.drive_for(REVERSE, 200, MM)
-// User Control Mode
-def ondriver_drivercontrol_0():
-    global myVariable
+def pre_auntonomus():
+# actions to do when program starts
+brain.screen.clear_screen()
+brain.screen.print ("pre auton code")
+wait(1,SECONDS)
+Motor_l1.spin(FORWARD)
+Motor_l2.spin(FORWARD)
+Motor_l3.spin(FORWARD)
+Motor_r1.spin(FORWARD)
+Motor_r2.spin(FORWARD)
+Motor_r3.spin(FORWARD)
+
+
+def autonomonus():
+    brain.screen.clear_screen()
+    brain.screen.print("autonomus code")
+    # place auntomonus code here
+
+def user_control():
+    brain.screen.clear_screen()
+    # place driver control code here
     while True:
-        wait(5, MSEC)
-// Controls for R1 & R2
-def onevent_controller_1buttonR1_pressed_0():
-    global myVariable
-    digital_out_a.set(True)
-
-def onevent_controller_1buttonR2_pressed_0():
-    global myVariable
-    digital_out_a.set(False)
-def onevent_controller_1buttonL1_pressed_0()
-    
+        wait(20, MSEC)
+        def drive_control():
+    if (controller.axis2.position() > 30):
+        drivetrain.drive(FORWARD, controller.axis2.position())
+    elif (controller.axis2.position() < -30):
+        drivetrain.drive(REVERSE,-controller.axis2.position())
+    else:
+        drivetrain.stop()
 
 
-
-
-// Autonumous Period
-def onauton_autonomous_0():
-    global myVariable
-    pass
-
-// create a function for handling the starting and stopping of all autonomous tasks
-def vexcode_auton_function():
-    # Start the autonomous control tasks
-    auton_task_0 = Thread( onauton_autonomous_0 )
-    # wait for the driver control period to end
-    while( competition.is_autonomous() and competition.is_enabled() ):
-        # wait 10 milliseconds before checking again
-        wait( 10, MSEC )
-    // Stop the autonomous control tasks
-    auton_task_0.stop()
-
-def vexcode_driver_function():
-    // Start the driver control tasks
-    driver_control_task_0 = Thread( ondriver_drivercontrol_0 )
-
-    # wait for the driver control period to end
-    while( competition.is_driver_control() and competition.is_enabled() ):
-        # wait 10 milliseconds before checking again
-        wait( 10, MSEC )
-    # Stop the driver control tasks
-    driver_control_task_0.stop()
-
-
-// register the competition functions
-competition = Competition( vexcode_driver_function, vexcode_auton_function )
-
-// system event handlers
-controller_1.buttonR1.pressed(onevent_controller_1buttonR1_pressed_0)
-controller_1.buttonR2.pressed(onevent_controller_1buttonR2_pressed_0)
-# add 15ms delay to make sure events are registered correctly.
-wait(15, MSEC)
-
-when_started1()
-
-
-
-        
+# create competition instance
+comp = Competition(user_control, autonomus)
+pre_autonomus()
