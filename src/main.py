3b3d38e886f65dc2777aa09add6f8f7b547de0f3
Razz1027 <boyillasaanvi8@gmail.com>
2024-10-26 14:28:17 -0700

    testing whether the commit actually commits our code properly?

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       Saanvi Boyilla                                               #
-# 	Created:      8/25/2024, 7:43:49 PM                                        #
-# 	Description:  V5 project                                                   #
+#   Module:       main.py                                                      #
+#   Author:       saanviboyilla                                                #
+#   Created:      10/20/2024, 5:14:58 PM                                       #
+#   Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
@@ -15,32 +15,35 @@ brain=Brain()
 
 brain.screen.print("Hello V5")
 
-motor_r1 = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)
-motor_r2 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
-motor_r3 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
+
+motor_r1 = Motor(Ports.PORT4, GearSetting.RATIO_6_1, True)
+motor_r2 = Motor(Ports.PORT5, GearSetting.RATIO_6_1, True)
+motor_r3 = Motor(Ports.PORT6, GearSetting.RATIO_6_1, True)
 right_drive = MotorGroup(motor_r1, motor_r2, motor_r3)
 
-motor_intake = Motor(Ports. PORT7, GearSetting. Ratio_6_1, False))
+motor_intake = Motor(Ports. PORT7, GearSetting.RATIO_6_1,False)
 
-motor_l1 = Motor(Ports.PORT4, GearSetting.RATIO_6_1, False)
-motor_l2 = Motor(Ports.PORT5, GearSetting.RATIO_6_1, False)
-motor_l3 = Motor(Ports.PORT6, GearSetting.RATIO_6_1, False)
+motor_l1 = Motor(Ports.PORT1, GearSetting.RATIO_6_1, False)
+motor_l2 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
+motor_l3 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, False)
 left_drive = MotorGroup(motor_l1, motor_l2, motor_l3)
 
-drivetrain = (left_drive, right_drive)
-
+drivetrain = DriveTrain(left_drive, right_drive)
+drivetrain.set_drive_velocity(100, PERCENT)
+drivetrain.set_turn_velocity(100, PERCENT)
 
 def pre_auntonomus():
 # actions to do when program starts
-brain.screen.clear_screen()
-brain.screen.print ("pre auton code")
-wait(1,SECONDS)
-Motor_l1.spin(FORWARD)
-Motor_l2.spin(FORWARD)
-Motor_l3.spin(FORWARD)
-Motor_r1.spin(FORWARD)
-Motor_r2.spin(FORWARD)
-Motor_r3.spin(FORWARD)
+    brain.screen.clear_screen() 
+    brain.screen.print ("pre auton code")
+    wait(1,SECONDS)
+    motor_l1.spin(FORWARD)
+    motor_l2.spin(FORWARD)
+    motor_l3.spin(FORWARD)
+    motor_r1.spin(FORWARD)
+    motor_r2.spin(FORWARD)
+    motor_r3.spin(FORWARD)
+    drivetrain.stop()
 
 
 def autonomonus():
@@ -48,20 +51,53 @@ def autonomonus():
     brain.screen.print("autonomus code")
     # place auntomonus code here
 
+
+controller = Controller()
+
 def user_control():
     brain.screen.clear_screen()
     # place driver control code here
     while True:
+        drive_control()
         wait(20, MSEC)
-        def drive_control():
-    if (controller.axis2.position() > 30):
-        drivetrain.drive(FORWARD, controller.axis2.position())
-    elif (controller.axis2.position() < -30):
-        drivetrain.drive(REVERSE,-controller.axis2.position())
+def drive_control():
+# Forward
+    if (controller.axis3.position() > 30): 
+        left_velocity = controller.axis3.position()
+        right_velocity = controller.axis3.position()
+        if (controller.axis1.position() > 30):
+        # veer right
+            right_velocity = right_velocity - controller.axis1.position()
+        elif (controller.axis1.position() < -30):
+            # veer left
+            left_velocity = left_velocity + controller.axis1.position()
+        right_drive.spin(REVERSE, right_velocity, PERCENT)
+        left_drive.spin(REVERSE, left_velocity, PERCENT)
+# Reverse
+    elif (controller.axis3.position() < -30):  
+        left_velocity = controller.axis3.position()
+        right_velocity = controller.axis3.position()
+        if (controller.axis1.position() < -30):
+        # veer right
+            right_velocity = right_velocity - controller.axis1.position()
+        elif (controller.axis1.position() > 30):
+            # veer left
+            left_velocity = left_velocity + controller.axis1.position()
+        right_drive.spin(REVERSE, right_velocity, PERCENT)
+        left_drive.spin(REVERSE, left_velocity, PERCENT) 
+# Left Turn
+    elif (controller.axis1.position() > 30): 
+       drivetrain.turn(LEFT, 25, PERCENT)
+# Right Turn
+    elif (controller.axis1.position() < -30): 
+        drivetrain.turn(RIGHT, 25, PERCENT)
     else:
         drivetrain.stop()
 
 
+
+
+
 # create competition instance
-comp = Competition(user_control, autonomus)
-pre_autonomus()
+comp = Competition(user_control, autonomonus)
+pre_auntonomus()
